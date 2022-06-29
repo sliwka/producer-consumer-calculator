851809556ce7f1504d3acb0e9160c30cebe68e8f
msliwka <msliwka@gmail.com>
2022-06-29 15:47:46 +0200

    refactoring

--- src/main/java/org/example/domain/Symbols.java
+++ src/main/java/org/example/domain/Symbols.java
@@ -4,7 +4,4 @@ public class Symbols {
     public static final String ADDITIVE_OPERATORS = "+-";
     public static final String MULTIPLICATIVE_OPERATORS = "*/";
     public static final String OPERATORS = ADDITIVE_OPERATORS + MULTIPLICATIVE_OPERATORS;
-    public static final String DIGITS_WITHOUT_ZERO = "123456789";
-    public static final String DIGITS = "0" + DIGITS_WITHOUT_ZERO;
-    public static final String SYMBOLS = OPERATORS + DIGITS;
 }
